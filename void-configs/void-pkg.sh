850bfec9026172b9fec2273199f4bf3ed94026e6
uxhen <farah.ushen@gmail.com>
2025-09-14 00:30:19 +0100

    comment

--- void-configs/void-pkg.sh
+++ void-configs/void-pkg.sh
@@ -3,7 +3,7 @@
 # Install Packages:                                                               #
 # =============================================================================== #
 sudo xbps-install -Sy \
-    # Intel Drivers:
+    # DRIVERS:
     vulkan-loader mesa-vulkan-intel intel-video-accel \
     # SYSTEM:
     base-devel elogind xtools mesa-dri \
@@ -11,31 +11,31 @@ sudo xbps-install -Sy \
     alacritty nushell carapace \
     # COMPOSITOR:
     river sandbar gtklock swayidle mako wmenu wlr-randr wvkbd wbg \
-    # SCREENSHARING TOOLS:
+    # SCREENSHARING:
     grim slurp swappy obs xdg-desktop-portal-wlr \
-    # COMMAND LINE TOOLS:
+    # CLI
     xdg-user-dirs xdg-utils psmisc man-db opendoas trash-cli htop aria2 ffmpeg ImageMagick ouch \
     yazi fastfetch yt-dlp chafa rsync eza bat glow starship wl-clipboard tealdeer  \
     neovim lazygit fzf fd ripgrep zoxide pastel delta curl jq brightnessctl \
-    # Graphical TOOLS:
+    # GUI
     mpv imv zathura zathura-pdf-mupdf \
     chromium qutebrowser nemo gimp shotcut \
-    # STORAGE TOOLS:
+    # DISKS:
     udiskie simple-mtpfs WoeUSB-cli \
     gvfs gvfs-mtp gvfs-smb gvfs-afc gvfs-gphoto2 \
-    # NETWORKING TOOLS:
+    # NETWORK:
     NetworkManager network-manager-applet linux-wifi-hotspot \
     net-tools wireless_tools bind-utils iputils inetutils-telnet \
-    # WAYLAND QT Tools:
+    # QT:
     qt5ct qt6ct qt5-wayland qt6-wayland kvantum  \
-    # SOUNDS TOOLS:
+    # SOUND:
     pipewire wireplumber alsa-pipewire alsa-utils pamixer pavucontrol \
-    # BLUETOOTHS TOOLS
+    # BLUETOOTH: 
     bluez bluez-alsa libspa-bluetooth blueman \
-    # LANGUAGE TOOLS:
+    # PROGRAMMING:
     nodejs lua-language-server  \
     # FONTS:
     noto-fonts-ttf noto-fonts-ttf-extra noto-fonts-emoji noto-fonts-cjk \
     font-awesome dejavu-fonts-ttf \
-    # theme
+    # THEMES
     gtk-engine-murrine breeze-hacked-cursor-theme papirus-icon-theme
