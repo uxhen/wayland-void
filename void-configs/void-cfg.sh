0845731235d6b9a12940205db78259a59aa81360
uxhen <farah.ushen@gmail.com>
2025-09-14 14:13:51 +0100

    clean config file

--- void-configs/void-cfg.sh
+++ void-configs/void-cfg.sh
@@ -1,59 +1,50 @@
 #!/bin/sh
-echo " ##################################################################"
-echo " #############   The Environment Is Ready  ########################"
-echo " ##################################################################"
+echo "Environment: ==============================================================================="
 rm -rf ~/.config 2>/dev/null && mkdir -p ~/{.config,.local/share}
 cd && mv wayland-void ~/.local/
 
-echo "#########################################"
-echo "###### Link My Dotfiles Programme #######"
-echo "#########################################"
-echo "===========> In ~/.config <================"
+echo "In ~/.config: =============================================================================="
 for config in $(ls $HOME/.local/wayland-void/void-dotfiles/cfg); do
     ln -sf ~/.local/wayland-void/void-dotfiles/cfg/$config ~/.config/
 done
-echo "=========> In ~/.local/share <============="
+
+echo "In ~/.local/share: ========================================================================="
 ln -sf ~/.local/wayland-void/void-dotfiles/fonts ~/.local/share/
 ln -sf ~/.local/wayland-void/void-dotfiles/themes ~/.local/share/
 ln -sf ~/.local/wayland-void/void-dotfiles/icons ~/.local/share/
-echo "===============> In ~/ <==================="
+
+echo "In ~/: ====================================================================================="
 ln -sf ~/.local/wayland-void/void-dotfiles/bsh/.bashrc ~/
 ln -sf ~/.local/wayland-void/void-dotfiles/bsh/.bash_profile ~/
 ln -sf ~/.local/wayland-void/void-dotfiles/others/.npmrc ~/
 ln -sf ~/.local/wayland-void/void-dotfiles/git/.gitconfig ~/
-echo "============> In /etc ~/ <================="
-# For Boot Slince And Hide All Logs Messages: #
-#=============================================#
+
+echo "In /etc: ==================================================================================="
 sudo ln -sf ~/.local/wayland-void/void-dotfiles/etc/grub/grub /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-# echo "#########################################"
-# echo "######    Directory In HOME       #######"
-# echo "#########################################"
+
+echo "HOME: ======================================================================================"
 LC_ALL=C.UTF-8 xdg-user-dirs-update --force 2> /dev/null
 mkdir -p ~/{Projects,Pictures,Desktop,Documents,Downloads,Music,Public,Videos}
 ln -sf ~/.local/wayland-void/void-dotfiles/pix ~/Pictures/
 
-echo "########################################"
-echo "######     managment Services   ########"
-echo "########################################"
+echo "Services: =================================================================================="
 sudo ln -s /etc/sv/elogind /var/service/
 sudo ln -s /etc/sv/dbus /var/service/
 sudo ln -s /etc/sv/NetworkManager /var/service/
 sudo ln -s /etc/sv/bluetoothd /var/service/
 
-echo "########################################"
-echo "#####     Others Configurations  #######"
-echo "########################################"
-# Pipewire Wireplumper pipewire-pulse alsa-pipewire: #
-#====================================================#
-## Wireplumper And Pipweire-pulse
+echo "Wireplumper: ==============================================================================="
 sudo mkdir -p /etc/pipewire/pipewire.conf.d
 sudo ln -s /usr/share/examples/wireplumber/10-wireplumber.conf /etc/pipewire/pipewire.conf.d/
+
+echo "Pulse: ====================================================================================="
 sudo ln -s /usr/share/examples/pipewire/20-pipewire-pulse.conf /etc/pipewire/pipewire.conf.d/
-## Alsa
+
+echo "Alsa: ======================================================================================"
 sudo mkdir -p /etc/alsa/conf.d
 sudo ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
 sudo ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
-# Using doas Like sudo Without Password: #
-#========================================#
+
+echo "doas: ======================================================================================"
 sudo bash -c "echo 'permit nopass lli as root' > /etc/doas.conf"
